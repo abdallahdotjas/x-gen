841875d8fe032f2df91e6960615037d6c25d49c1
jashaikh-dev <junaidazharshaikh@gmail.com>
2020-03-06 23:30:28 -0500

    refactor: create to operations

--- src/index.js
+++ src/index.js
@@ -4,31 +4,7 @@
 // pass method and object to the function 
 // 
 const dbConfig = require('./config.json')
-
-function create(tableName, columnsObject) {
-    return generateCreateTableStatement( tableName, createArrayOfColumns(columnsObject));
-}
-
-function createArrayOfColumns(columnsObject) {
-    const columns = []
-    for(const key in columnsObject) {
-        const column = columnsObject[key]
-        const columnDetails = []
-        columnDetails.push(key.toLowerCase())
-        column.type == 'string' 
-            ? columnDetails.push(`VARCHAR ${(column.size)}`)
-            : columnDetails.push(`${ column.type }`)
-        if(column.constraints && column.constraints.length)
-            column.constraints.forEach(item => columnDetails.push(`${ item.toUpperCase() }`))
-        columns.push(columnDetails.join(" "));
-    }
-    return columns
-}
-
-function generateCreateTableStatement(tableName,columns){
-    return(`CREATE TABLE ${tableName} ( ${columns.toString()} );`)
-}
-
+const create = require('./operations/create')
 module.exports = {
     create
 }
---
+++ src/operations/create.js
@@ -0,0 +1,27 @@
+
+
+module.exports  = function create(tableName, columnsObject) {
+  return generateCreateTableStatement( tableName, createArrayOfColumns(columnsObject));
+}
+
+function createArrayOfColumns(columnsObject) {
+  const columns = []
+  for(const key in columnsObject) {
+      const column = columnsObject[key]
+      const columnDetails = []
+      columnDetails.push(key.toLowerCase())
+      column.type == 'string' 
+          ? columnDetails.push(`VARCHAR ${(column.size)}`)
+          : columnDetails.push(`${ column.type }`)
+      if(column.constraints && column.constraints.length)
+          column.constraints.forEach(item => columnDetails.push(`${ item.toUpperCase() }`))
+      columns.push(columnDetails.join(" "));
+  }
+  return columns
+}
+
+function generateCreateTableStatement(tableName,columns){
+  return(`CREATE TABLE ${tableName} ( ${columns.toString()} );`)
+}
+
+
