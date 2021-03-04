41247b303b687278c17912a3c6112bb05415ee7d
abdallahdotjs <junaidazharshaikh@gmail.com>
2021-03-03 22:19:07 -0500

    chore: test command

--- src/bin/index.js
+++ src/bin/index.js
@@ -3,3 +3,24 @@
 const chalk = require('chalk');
 const boxen = require('boxen');
 const yargs = require('yargs');
+
+
+const options = yargs
+  .usage("Usage: -n <name>")
+  .option("n", {alias: "name", describe: "Your name", type: "string", demandOption: true })
+  .option("s", { alias: "search", describe: "Search term", type: "string"})
+  .argv;
+
+const greeting = chalk.white.bold(`Name:  ${options.name}`);
+
+const boxenOptions = {
+  padding: 1,
+  margin: 1, 
+  borderStyle: "round",
+  borderColor: "green",
+  backgroundColor: "#555555"
+};
+
+const msgBox = boxen( greeting, boxenOptions);
+
+console.log(msgBox);
